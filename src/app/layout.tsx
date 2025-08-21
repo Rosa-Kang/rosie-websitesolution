4ac065cb514a41da5af732e23965bbc16294ad7c
Rosa Kang <therosessom@gmail.com>
2025-08-21 11:32:04 -0400

    update the favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Therosessom - Premium Web Design Studio',
   description: 'We create strategically built Shopify & Squarespace websites for female-led brands who are ready to flourish.',
+  icons: {
+    icon: '/images/favicon.ico',
+  },
 }
 
 export default function RootLayout({
